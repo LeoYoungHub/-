7453d0de9c58a1165e77a70c4d5265dd407de3ee
Leo <yanglei383800043@126.com>
2020-12-14 17:15:15 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace 爬虫
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBoxEx1 = new HZH_Controls.Controls.TextBoxEx();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.textBoxEx1);
+			this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+			this.splitContainer1.Panel1.Controls.Add(this.button2);
+			this.splitContainer1.Panel1.Controls.Add(this.button4);
+			this.splitContainer1.Panel1.Controls.Add(this.button3);
+			this.splitContainer1.Panel1.Controls.Add(this.button1);
+			this.splitContainer1.Size = new System.Drawing.Size(1092, 793);
+			this.splitContainer1.SplitterDistance = 226;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Location = new System.Drawing.Point(3, 126);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(220, 664);
+			this.listBox1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(23, 54);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "调试";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(119, 54);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "打开";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(119, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "测试";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(23, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "登录";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBoxEx1
+			// 
+			this.textBoxEx1.DecLength = 2;
+			this.textBoxEx1.InputType = HZH_Controls.TextInputType.NotControl;
+			this.textBoxEx1.Location = new System.Drawing.Point(0, 99);
+			this.textBoxEx1.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+			this.textBoxEx1.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+			this.textBoxEx1.MyRectangle = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.textBoxEx1.Name = "textBoxEx1";
+			this.textBoxEx1.OldText = null;
+			this.textBoxEx1.PromptColor = System.Drawing.Color.Gray;
+			this.textBoxEx1.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.textBoxEx1.PromptText = "";
+			this.textBoxEx1.RegexPattern = "";
+			this.textBoxEx1.Size = new System.Drawing.Size(223, 21);
+			this.textBoxEx1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1092, 793);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button4;
+		private HZH_Controls.Controls.TextBoxEx textBoxEx1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace 爬虫
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			InitBrowser();
+		}
+
+		string domian = "https://www.kaoshibao.com/online/?paperId=652822&practice=&modal=1&is_recite=&ptype=&text=%E9%A1%BA%E5%BA%8F%E7%BB%83%E4%B9%A0&sequence=0";// "https://www.kaoshibao.com/";
+		public ChromiumWebBrowser chromiumWebBrowser;
+		public void InitBrowser()
+		{
+			Cef.Initialize(new CefSettings());
+			chromiumWebBrowser = new ChromiumWebBrowser(domian);
+			splitContainer1.Panel2.Controls.Add(chromiumWebBrowser);
+			chromiumWebBrowser.Dock = DockStyle.Fill;
+			chromiumWebBrowser.FrameLoadEnd += webbrowser_FrameLoadEnd;
+
+
+		}   /// 回调事件
+
+		string cookies = string.Empty;
+		void visitor_SendCookie(CefSharp.Cookie obj)
+		{
+			cookies += obj.Domain.TrimStart('.') + "^" + obj.Name + "^" + obj.Value + "$";
+		}
+
+		async void setCookies()
+		{
+			var cookieManager = CefSharp.Cef.GetGlobalCookieManager();
+			await cookieManager.SetCookieAsync(domian, new CefSharp.Cookie
+			{
+				Domain = domian,
+				Name = "kaoshibao",
+				Value = "kaoshibao",
+				Expires = DateTime.MinValue
+			});
+		}
+
+
+		async void webbrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+		{
+			string _url = e.Url;
+			//判断是否是需要获取cookie的页面
+			if (_url.Contains(domian))
+			{
+				//注册获取cookie回调事件
+
+				var visitor = new CookieMonster();
+				//var list = visitor.GetCookieList("www.kaoshibao.com");
+				var isSucess = visitor.setCookie("www.kaoshibao.com", "ceshi", "123456", true);
+			}
+
+
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			string name = "18126015434";
+			string pwd = "yl123456";
+			string nameClassid = "el-input__inner";
+			//document.querySelector("#body > div > div.layout-container.prative-page > div.prative-box > div.topic.no-select > div > div.select-left.pull-left.options-w")
+			//document.querySelector("#body > div > div.layout-container.prative-page > div.prative-box > div.topic.no-select > div > div.topic-top > div > div")
+			CefWebBrowserControl.ClickButtonByJsPath(chromiumWebBrowser, @"document.querySelector('#body > div > div > div.form-main > form > div > div:nth-child(1) > div > div > div > input').value='18126015434'");
+			CefWebBrowserControl.ClickButtonByJsPath(chromiumWebBrowser, @"document.querySelector('#body > div > div > div.form-main > form > div > div:nth-child(2) > div > div > div > input').value='yl123456'");
+			CefWebBrowserControl.ClickButtonByJsPath(chromiumWebBrowser, @"document.querySelector('#body > div > div > div.form-main > form > div > div.mb20 > button').click()");
+
+
+
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			chromiumWebBrowser.GetBrowser().ShowDevTools();
+		}
+
+		private async void button3_Click(object sender, EventArgs e)
+		{
+			CefWebBrowserControl.ClickButtonByJsPath(chromiumWebBrowser, @"document.querySelector('div.tool-bar').children[6].click()");
+
+			while (true)
+			{
+				await Task.Delay(500);
+				string html = await CefWebBrowserControl.GetHtmlSource(chromiumWebBrowser);
+
+				HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+				doc.LoadHtml(html);//html字符串
+								   //获取标签里的值select-left pull-left options-w
+
+				var enable = doc.DocumentNode.SelectSingleNode("//div[@class='next-preve']/button[2]");
+				if (enable.Attributes["disabled"] != null && enable.Attributes["disabled"].Value.Equals("disabled"))
+				{
+					break;
+				}
+
+				var topic_type = doc.DocumentNode.SelectSingleNode("//span[@class='topic-type']").InnerText;
+				var topic = doc.DocumentNode.SelectSingleNode("//div[@class='qusetion-box']").InnerText;
+
+				if ("单选题 多选题 判断题 不定项选择题 排序题".Contains(topic_type))
+				{
+					var answers = doc.DocumentNode.SelectNodes("//div[@class='option']");
+
+					listBox1.Items.Add(topic_type);
+					listBox1.Items.Add(topic);
+					foreach (var item in answers)
+					{
+						string value1 = item.SelectNodes("./span")[0].InnerText;
+						string value2 = item.SelectNodes("./span")[1].InnerText;
+
+						listBox1.Items.Add($"{value1}{value2}");
+					}
+				}
+
+				var answerRight = doc.DocumentNode.SelectSingleNode("//p[@class='answer-right']/span").InnerText;
+				var answerAnalysis = doc.DocumentNode.SelectSingleNode("//p[@class='answer-analysis']").InnerText;
+				listBox1.Items.Add("答案:" + answerRight);
+				listBox1.Items.Add("注释:" + answerAnalysis);
+
+				CefWebBrowserControl.ClickButtonByJsPath(chromiumWebBrowser, @"document.querySelector('div.next-preve').children[1].click()");
+			}
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			CefWebBrowserControl.ClickButtonByJsPath(chromiumWebBrowser, @"document.querySelector('div.tool-bar').children[6].click()");
+		}
+	}
+
+	/// <summary>
+	/// CefSharp的cookie的操作类
+	/// </summary>
+	public class CookieMonster : ICookieVisitor
+	{
+		public List<CefSharp.Cookie> cookies = new List<CefSharp.Cookie>();
+		readonly ManualResetEvent gotAllCookies = new ManualResetEvent(false);
+
+
+		/// <summary>
+		/// 获取cookie
+		/// </summary>
+		/// <param name="DomainStr">根据域名获取，如果DomainStr为空则获取所有的cookie</param>
+		/// <returns></returns>
+		public List<CefSharp.Cookie> GetCookieList(string DomainStr = "")
+		{
+			var visitor = new CookieMonster();
+			if (DomainStr.Length > 0)
+			{
+				var cookieManager = CefSharp.Cef.GetGlobalCookieManager();
+				if (cookieManager.VisitAllCookies(visitor))
+				{
+					visitor.WaitForAllCookies();
+					return visitor.cookies.Where(p => p.Domain == DomainStr || p.Domain == "." + DomainStr).ToList();
+				}
+				else
+				{
+					return visitor.cookies;
+				}
+			}
+			else
+			{
+				var cookieManager = CefSharp.Cef.GetGlobalCookieManager();
+				if (cookieManager.VisitAllCookies(visitor))
+					visitor.WaitForAllCookies();
+				return visitor.cookies;
+			}
+		}
+
+		/// <summary>
+		/// 给浏览器设置cookie
+		/// </summary>
+		/// <returns></returns>
+		public async Task<bool> setCookie(string domainStr, string nameStr, string valueStr, bool ishttps)
+		{
+			string httpStr = "http";
+			if (ishttps)
+			{
+				httpStr = "https";
+			}
+			var cookieManager = CefSharp.Cef.GetGlobalCookieManager();
+			var bol = await cookieManager.SetCookieAsync(httpStr + "://" + domainStr, new CefSharp.Cookie()
+			{
+				Domain = domainStr,
+				Name = nameStr,
+				Value = valueStr,
+				Path = "/",
+				HttpOnly = true,
+				Expires = DateTime.Now.AddMinutes(1)
+			});
+			return bol;
+		}
+
+		public bool Visit(CefSharp.Cookie cookie, int count, int total, ref bool deleteCookie)
+		{
+
+			cookies.Add(cookie);
+			if (count == total - 1)
+				gotAllCookies.Set();
+			return true;
+		}
+
+		public void WaitForAllCookies()
+		{
+			gotAllCookies.WaitOne();
+		}
+
+		public void Dispose()
+		{
+			//cookies.Clear();
+		}
+	}
+}
